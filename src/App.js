1fdb2ab6422ff350c019ef87ed3830ba5fb23633
AnimeshKatigar <katigaranimesh@gmail.com>
2021-08-11 20:12:40 +0530

    setting up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+import Header from "./components/header";
+import List from "./components/list";
+import Map from "./components/map";
+import Details from "./components/details";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Kasa ahes?</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
